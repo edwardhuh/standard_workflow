8959e5a7f9499db5637a295a3bad31c9a404b06a
Edward Huh <edward_huh@brown.edu>
2020-10-28 16:37:44 -0500

    including refactoring and testing for wiki

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,6 @@
 import click.testing
 import pytest
+import requests
 
 from standard_workflow import console
 
@@ -13,23 +14,44 @@ def runner():
 # including a pytest fixture that adds a function parameter to test case
 @pytest.fixture
 def mock_requests_get(mocker):
-# even in the mock fixture, you need to give it the proper return value.
-	mock = mocker.patch("requests.get")
-	mock.return_value.__enter__.return_value.json.return_value = {
-		"title": "Lorem",
-		"extract": "stuff stuff stuff",
-	}
-	return mock
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem",
+        "extract": "stuff stuff stuff",
+    }
+    return mock
 
+@pytest.fixture
+def mock_requests_get(mocker):
+    return mocker.patch("requests.get")
 
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
 
-# The below code has been simplified using a pytest fixture
-# def test_main_succeeds():
-#     runner = click.testing.CliRunner()
-#     ## the testing.CliRunner can invoke the cli from within a test case!!!
-#     result = runner.invoke(console.main)
-#     assert result.exit_code == 0
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
+
+def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
+
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception("Boom")
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
+
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("standard_workflow.wikipedia.random_page")
+
+
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
